f95f30cd10f99754b92e71ee2039d3808934a679
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-11-24 17:57:56 -0500

    added few prices at top, touch of styling to price displays

--- crypto_sphere/crypto_sphere/views.py
+++ crypto_sphere/crypto_sphere/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render
 def home_page(request):
     import requests
     import json
+
+    #crypto prices
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,XRP,TRON&tsyms=USD")
+    price = json.loads(price_request.content)
+
+
+    # crypto news
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
-    return render(request, 'home.html', {'api': api})
+    return render(request, 'home.html', {'api': api, 'price':price})
